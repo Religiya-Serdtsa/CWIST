c96af6c59d17a1034d2dc8c093e108e3f74d301b
Lee Yunjin <gzblues61@gmail.com>
2026-07-04 17:34:52 +0900

    revert: rollback h2_write_all to its original simple implementation

--- src/net/http/http2.c
+++ src/net/http/http2.c
@@ -230,25 +230,7 @@ static int h2_write_all(cwist_https_connection *conn, const void *buf, size_t le
     const unsigned char *p = (const unsigned char *)buf;
     while (len > 0) {
         int n = h2_write(conn, p, (int)len);
-        if (n <= 0) {
-            if (n < 0) {
-                if (conn->ssl) {
-                    int err = SSL_get_error(conn->ssl, n);
-                    if (err == SSL_ERROR_WANT_WRITE || err == SSL_ERROR_WANT_READ) {
-                        struct timespec ts = {0, 1000000}; // 1ms sleep
-                        nanosleep(&ts, NULL);
-                        continue;
-                    }
-                } else {
-                    if (errno == EAGAIN || errno == EWOULDBLOCK || errno == EINTR) {
-                        struct timespec ts = {0, 1000000}; // 1ms sleep
-                        nanosleep(&ts, NULL);
-                        continue;
-                    }
-                }
-            }
-            return -1;
-        }
+        if (n <= 0) return -1;
         p += n;
         len -= (size_t)n;
     }
